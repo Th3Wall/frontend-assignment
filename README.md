7602783fd82e38873cd310cc0090d38757e0917d
Davide Mandelli <mandellidavide.95@gmail.com>
2022-04-14 02:29:40 +0200

    Client partially configured

--- README.md
+++ README.md
@@ -9,18 +9,18 @@ It is not important to have a beautiful UI in terms of design, but we need to ha
 ## Introduction
 
 For this assignment what you will build is a simple page (and a server app) for querying and filtering a dataset of Pokémons.  
-The Pokèmons dataset is harcoded inside the project.
+The Pokèmons dataset is hardcoded inside the project.
 
 The server app will be powered by Node.js, `typescript` and [`apollo-server`](https://www.apollographql.com/docs/apollo-server/) and it is partially implemented.  
 The client app will be powered by `typescript`, `react`, [`apollo-client`](https://www.apollographql.com/docs/react/) and [`antd`](https://ant.design/) and you will build it from scratch.
 
-You can add other libraries if needed; if you are not confident with some of this libraries you can pick alternatives.  
+You can add other libraries if needed; if you are not confident with some of these libraries you can pick alternatives.  
 For the client side app we recommend to use `webpack`.
 
 This project is powered by `yarn` and workspaces.  
 Once cloned this project can be installed with `yarn install`.  
 You can run scripts on client with `yarn workspace @frontend-assignment/client <command>`, and on server with `yarn workspace @frontend-assignment/server <command>`.  
-For the server you can start the the app with `yarn workspace @frontend-assignment/server start`.
+For the server you can start the app with `yarn workspace @frontend-assignment/server start`.
 
 ## Tasks
 
---
+++ packages/client/webpack/webpack.dev.config.ts
@@ -0,0 +1,72 @@
+import * as path from "path";
+import { Configuration as WebpackConfiguration, HotModuleReplacementPlugin } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import HtmlWebpackPlugin = require("html-webpack-plugin");
+import ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+import ESLintPlugin from "eslint-webpack-plugin";
+
+interface Configuration extends WebpackConfiguration {
+    devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+    mode: "development",
+    output: {
+        publicPath: "/",
+    },
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+        }),
+        new HotModuleReplacementPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            async: false
+        }),
+        new ESLintPlugin({
+            extensions: ["js", "jsx", "ts", "tsx"],
+        })
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+        static: path.join(__dirname, "build"),
+        historyApiFallback: true,
+        port: 3000,
+        open: true,
+        hot: true
+    },
+};
+
+export default config;
